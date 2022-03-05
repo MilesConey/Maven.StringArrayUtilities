1f8fb8b83054ac38f6ccae28e3b1e2309718edde
Miles Coney <MilesEConey@gmail.com>
2022-03-05 01:39:42 -0500

    Completed up to pangramic

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -9,7 +11,11 @@ public class StringArrayUtils {
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
-        return array[0];
+        if (array.length >= 1) { // checking that there is at least one or more elements in the array.
+        return array[0]; //if true, return first element of array. I couldve just returned this line, but in case i am given an empty array, i am taking into account that it would return null
+        } else {
+            return null; // returns null when first statement is false because there are no elements in the array
+        }
     }
 
     /**
@@ -17,15 +23,21 @@ public class StringArrayUtils {
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
+        if(array.length >=1){
         return array[1];
+        }
+        else {
+          return null;
+        }
     }
 
     /**
-     * @param array array of String objects
+     * @param array array of String objects // I want to get last element of array. Can use index
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+//        int lastElement = array.length - 1;
+        return array[array.length - 1]; // array. length - 1 will give me the last index in the array by taking the length of the array and subtracting 1. An alternative return could be return array[lastElement];
     }
 
     /**
@@ -33,7 +45,7 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length - 2];
     }
 
     /**
@@ -42,6 +54,11 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)) { //is checking entire array (by looping through the array) for value. If the array contains the value, it will return true. Otherwise false. originally had array[i] == value but intelliJ suggested the switch to .equals
+                return true;
+            }
+        }
         return false;
     }
 
@@ -50,7 +67,17 @@ public class StringArrayUtils {
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+        //array =["momo", "einstein","Miles", "Tati"] =4
+//                 0          1        2       3
+    //strArray = ["tati", "miles", "einstein", "momo"]=4
+//                   0        1        2          3
+        String[] strArray = new String[array.length]; //creating new array of strings that has the same length as the original string so that all elements of the original can be reversed in the new string array
+        int counter = 0;
+        for (int i = array.length - 1; i >= 0; i--) { //for loop runs backwards because we are reversing the array, for instance, the last element in the original array, will be looped through first and the first element in the array will be last.
+             strArray[counter] = array [i]; // this is setting the strArray = to array[i] so that they loop together, in opposite directions, which allow the array to be reversed
+             counter++; // incrementing the strArray
+        }
+        return strArray; // returning the reversed array
     }
 
     /**
@@ -58,15 +85,23 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
-        return false;
+        // array = ["hannah"]
+        String[] strArray = reverse(array);
+
+        return Arrays.equals(strArray, array);
     }
 
     /**
      * @param array array of String objects
      * @return true if each letter in the alphabet has been used in the array
+     * https://www.baeldung.com/java-string-contains-all-letters- used this resource
      */ // TODO
     public static boolean isPangramic(String[] array) {
-        return false;
+        String pangramic = Arrays.toString(array);
+        return pangramic.toLowerCase()
+                .replaceAll("[^a-z]", "")
+                .replaceAll("(.)(?=.*\\1)", "")
+                .length() == 26;
     }
 
     /**
